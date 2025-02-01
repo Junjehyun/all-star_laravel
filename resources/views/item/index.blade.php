5dfdf0c6db754e9e660da3e1a771c09323453096
ジェヒョン <h94051987@gmail.com>
2025-02-01 19:06:02 +0900

    [250201] Up Arrow 등록

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -57,6 +57,9 @@
             </div>
         @endforelse
     </div>
+    <div id="to-top">
+        <div class="material-icons">arrow_upward</div>
+    </div>
     <script>
         const initialKeyword = "{{ $keyword ?? '' }}"; // 서버에서 키워드 전달
 
@@ -137,5 +140,43 @@
                 }
             });
         }
+
+        //const badgeEl = document.querySelector('.badge');
+        const toTopEl = document.querySelector('#to-top');
+
+        window.addEventListener('scroll', _.throttle(function () {
+            console.log(window.scrollY);
+
+            if (window,scrollY > 500) {
+                // 배지 숨기기
+                // gsap.to(요소, 지속시간, 옵션);
+                // gsap.to(badgeEl, 0.6, {
+                //     opacity: 0,
+                //     display: 'none'
+                // });
+                // 버튼 보이기!
+                gsap.to(toTopEl, 0.2, {
+                    x: 0
+                });
+            } else {
+                // 배지 보이기
+                // gsap.to(badgeEl, 0.6, {
+                //     opacity: 1,
+                //     display: 'block'
+                // });
+                // 버튼 숨기기!
+                gsap.to(toTopEl, 0.2, {
+                    x: 100
+                });
+            }
+
+        }, 300));
+
+        // _.throttle(함수, 시간)
+        toTopEl.addEventListener('click', function () {
+        gsap.to(window, .7, {
+            scrollTo: 0
+        });
+    });
     </script>
 @endsection
--- resources/views/layouts/shop_common.blade.php
+++ resources/views/layouts/shop_common.blade.php
@@ -20,6 +20,13 @@
         @livewireStyles
         <!-- Alpine.js CDN -->
         <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
+        <!-- Lodash 라이브러리 추가 (버전은 필요에 따라 변경) -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js"></script>
+        <!-- GSAP 라이브러리 -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/gsap.min.js"></script>
+        <!-- GSAP ScrollToPlugin -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/ScrollToPlugin.min.js"></script>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <title>@yield('title') - xyz Mart</title>
         <style>
             body {
@@ -27,6 +34,36 @@
             }
             h1, h2, h3, h4, h5 {
                 font-family: "JetBrains Mono", "Kiwi Maru", sans-serif;
+            }
+
+            #to-top {
+                width: 42px;
+                height: 42px;
+                background-color: #333;
+                color: #fff;
+                border: 2px solid #fff;
+                border-radius: 10px;
+                cursor: pointer;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                position: fixed;
+                bottom: 30px;
+                right: 30px;
+                z-index: 9;
+            }
+
+            .material-icons {
+                height: 24px;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -60%);
+                margin: auto;
+                transition: 0.4s;
+                opacity: 1;
+                color: #fff;
+                font-size: 30px;
             }
         </style>
     </head>
