a723b13a16d4d70e5798c2b253d083d52452f6b4
ジェヒョン <h94051987@gmail.com>
2025-02-07 11:22:45 +0900

    [250207] 2. 주문내역 수정 완료

--- resources/views/order/tracking.blade.php
+++ resources/views/order/tracking.blade.php
@@ -115,4 +115,108 @@
             </div>
         @endif
     @endauth --}}
+    <div class="container w-full justify-center mx-auto">
+        <h1 class="text-center text-2xl font-bold mt-10">TRACKING DELIVERY</h1>
+        <p class="text-center mt-10">
+            <span class="{{ $shipping_status == 'pay-confirm' ? 'font-bold text-green-500 text-2xl' : '' }}">
+                @if($shipping_status == 'pay-confirm')
+                    <i class="fa-solid fa-check"></i>
+                @endif
+                CHECK PAYMENT
+            </span> &rarr;
+            <span class="{{ $shipping_status == 'prepare' ? 'font-bold text-green-500 text-2xl' : '' }}">
+                @if($shipping_status == 'prepare')
+                    <i class="fa-solid fa-check"></i>
+                @endif
+                PREPARING PRODUCT
+            </span> &rarr;
+            <span class="{{ $shipping_status == 'delivery' ? 'font-bold text-green-500 text-2xl' : '' }}">
+                @if($shipping_status == 'delivery')
+                    <i class="fa-solid fa-check"></i>
+                @endif
+                SHIPPING
+            </span> &rarr;
+            <span class="{{ $shipping_status == 'complete' ? 'font-bold text-green-500 text-2xl' : '' }}">
+                @if($shipping_status == 'complete')
+                    <i class="fa-solid fa-check"></i>
+                @endif
+                DELIVERY COMPLETE
+            </span>
+        </p>
+    </div>
+    <div class="w-[60%] flex justify-between items-stretch mt-20 mx-auto space-x-5">
+        <div class="w-1/2 border rounded-xl py-5">
+            <!-- 상품 정보 테이블 -->
+            <h2 class="text-2xl font-semibold text-center mt-2 mb-10">ORDER DETAILS</h2>
+            <table class="flex justify-center">
+                <tbody>
+                    <tr class="border-b">
+                        <td class="px-4 py-2 font-semibold text-left">ORDER ID</td>
+                        <td>{{ $order->id }}</td>
+                    </tr>
+                    <tr class="border-b">
+                        <td class="px-4 py-2 font-semibold text-left">ITEM</td>
+                        <td>{{ $order->item->name }}</td>
+                    </tr>
+                    <tr class="border-b">
+                        <td class="px-4 py-2 font-semibold text-left">QUANTITY</td>
+                        <td>{{ $order->quantity }}</td>
+                    </tr>
+                    <tr class="border-b">
+                        <td class="px-4 py-2 font-semibold text-left">SIZE</td>
+                        <td>{{ $order->item->size }}mm</td>
+                    </tr>
+                    <tr class="border-b">
+                        <td class="px-4 py-2 font-semibold text-left">PAYMENT TYPE</td>
+                        <td>{{ $order->payment_type }}</td>
+                    </tr>
+                    <tr class="border-b">
+                        <td class="px-4 py-2 font-semibold text-left">TOTAL</td>
+                        <td>¥{{ number_format($order->amount) }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="w-1/2 border rounded-xl py-5">
+            <!-- 주문고객 정보 테이블 -->
+            <h2 class="text-2xl font-semibold text-center mt-2 mb-10">CUSTOMER INFO</h2>
+            <table class="flex justify-center">
+                <tbody>
+                    <tr class="border-b">
+                        <td class="px-4 py-2 font-semibold text-left">NAME</td>
+                        <td>{{ $order->customer_name }}</td>
+                    </tr>
+                    <tr class="border-b">
+                        <td class="px-4 py-2 font-semibold text-left">TELEPHONE</td>
+                        <td>{{ $order->customer_phone }}</td>
+                    </tr>
+                    <tr class="border-b">
+                        <td class="px-4 py-2 font-semibold text-left">E-MAIL</td>
+                        <td>{{ $order->customer_email }}</td>
+                    </tr>
+                    <tr class="border-b">
+                        <td class="px-4 py-2 font-semibold text-left">ADDRESS</td>
+                        <td>{{ $order->customer_address }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    @auth
+        @if(Auth::user()->role === 'admin')
+            <div class="container w-1/2 mx-auto mt-20">
+                <form action="{{ route('update.shipping.status', $order->id) }}" method="POST">
+                    @csrf
+                    <label for="shipping_status">SELECT STATUS:</label>
+                    <select name="shipping_status" id="shipping_status">
+                        <option value="pay-confirm" {{ $order->shipping_status == 'pay-confirm' ? 'selected' : '' }}>PAID CONFIM</option>
+                        <option value="prepare" {{ $order->shipping_status == 'prepare' ? 'selected' : '' }}>PREPARE ITEM</option>
+                        <option value="delivery" {{ $order->shipping_status == 'delivery' ? 'selected' : '' }}>DELIVERY</option>
+                        <option value="complete" {{ $order->shipping_status == 'complete' ? 'selected' : '' }}>COMPLETE</option>
+                    </select>
+                    <button type="submit" class="outline outline-gray-200 py-2 px-2 rounded-xl">SAVE</button>
+                </form>
+            </div>
+        @endif
+    @endauth
 @endsection
